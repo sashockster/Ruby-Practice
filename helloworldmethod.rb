e1655fd84405993728ff893dc1d7b1b3670028b2
Shaina Shockley <shockley.shaina@gmail.com>
2017-06-29 00:51:29 +0000

    added playground

---
+++ helloworldmethod.rb
@@ -0,0 +1,5 @@
+def hello(anything)
+  puts anything
+end
+
+hello("Hello, my name is Shaina")
---
+++ playground.rb
@@ -0,0 +1,5 @@
+puts "Hello enter in your first name:"
+first_name = gets.chomp
+puts "What is your last_name"
+last_name = gets.chomp
+puts "Welcome #{first_name} #{last_name}, to the playground"
