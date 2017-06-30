4fc8d8f346aaf776c7db93dcb5e2ef3a69530667
Shaina Shockley <shockley.shaina@gmail.com>
2017-06-30 04:01:21 +0000

    added extra lines

--- analyzer.rb
+++ analyzer.rb
@@ -4,3 +4,4 @@ puts("Now, please enter your last name.")
 last_name = gets.chomp
 puts("So you're #{first_name}! Nice to meet you Mr./Mrs. #{last_name}.")
 puts("Did you know that your first name in reverse is #{first_name.reverse}.")
+puts("And the length of your first name is #{first_name.length}, while the length of your last name is #{last_name.length}. How cool! ")
