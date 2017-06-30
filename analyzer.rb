e1f8a842cc4f17d7f40f5a99250ea71ce7cdc669
Shaina Shockley <shockley.shaina@gmail.com>
2017-06-30 04:59:49 +0000

    else if is elsif? haha

--- analyzer.rb
+++ analyzer.rb
@@ -1,7 +1,48 @@
-puts("Welcome! Please enter your first name")
-first_name = gets.chomp
-puts("Now, please enter your last name.")
-last_name = gets.chomp
-puts("So you're #{first_name}! Nice to meet you Mr./Mrs. #{last_name}.")
-puts("Did you know that your first name in reverse is #{first_name.reverse}.")
-puts("And the length of your first name is #{first_name.length}, while the length of your last name is #{last_name.length}. How cool! ")
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f
+end
+
+def divide(first_number, second_number)
+  first_number.to_f / second_number.to_f
+end
+
+def subtract(first_number, second_number)
+  first_number.to_f - second_number.to_f
+end
+
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f
+end
+puts "What do you want to do? 1) multply 2) divide 3) subtract 4) find remainder"
+prompt = gets.chomp
+
+puts "Please enter your first number"
+first_number = gets.chomp
+puts "Enter a second number now"
+second_number = gets.chomp
+
+if prompt =='1'
+  puts "You have choose to multiply #{first_number} with #{second_number}"
+  result = multiply(first_number, second_number)
+  
+elsif prompt == '2'
+  puts "You have choosen to divide #{first_number} with #{second_number}"
+    result = divide(first_number, second_number)
+
+  
+elsif prompt == '3'
+  puts "You have choose to subtract #{first_number} with #{second_number}"
+    result = subtract(first_number, second_number)
+
+  
+elsif prompts == '4'
+  puts "You have choosen to find the remainder #{first_number} with #{second_number}"
+    result = mod(first_number, second_number)
+
+
+else
+  puts "You have choosen an invalid choice"
+  
+end
+
+puts "The result is #{result}"
--- playground.rb
+++ playground.rb
@@ -1,5 +1,7 @@
-puts "Hello enter in your first name:"
-first_name = gets.chomp
-puts "What is your last_name"
-last_name = gets.chomp
-puts "Welcome #{first_name} #{last_name}, to the playground"
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f
+  
+end
+
+puts multiply(5,10)
+puts multiply(8, 30)
