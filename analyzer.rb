3d59b52f22b70a33a961205c16a990c7d87e30ef
Shaina Shockley <shockley.shaina@gmail.com>
2017-06-30 03:57:39 +0000

    first project! :D

---
+++ analyzer.rb
@@ -0,0 +1,6 @@
+puts("Welcome! Please enter your first name")
+first_name = gets.chomp
+puts("Now, please enter your last name.")
+last_name = gets.chomp
+puts("So you're #{first_name}! Nice to meet you Mr./Mrs. #{last_name}.")
+puts("Did you know that your first name in reverse is #{first_name.reverse}.")
